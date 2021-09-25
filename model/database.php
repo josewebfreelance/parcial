b3a598b50a99aa69f25e922e75050aba54ba1986
Jose Lemus <jlemus@is4tech.com>
2021-09-25 13:04:56 -0600

    conexion

--- model/database.php
+++ model/database.php
@@ -3,7 +3,9 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=parcial22021.database.windows.net;dbname=parcial2;charset=utf8', 'parcial22021', 'parcial2#');
+        $pdo = new PDO("sqlsrv:server = tcp:parcial22021.database.windows.net,1433; Database = parcial2", "parcial22021", "parcial2#");
+
+        // $pdo = new PDO('mysql:host=parcial22021.database.windows.net;dbname=parcial22021;charset=utf8', 'parcial22021', 'parcial2#');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
